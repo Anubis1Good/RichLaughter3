68bec4dd011aa390483cdcf14f35f1157092ccf3
Anubis1Good <vlad.polyakov.96@inbox.ru>
2026-09-20 17:07:46 +0300

    some prepare

--- traders/VT/bot_on_ticker.py
+++ traders/VT/bot_on_ticker.py
@@ -57,111 +57,111 @@ bot_on_ticker = {
 
     #+1 +10 +10 !(35%) +0.4 +3.7 +2.4 -2.2 -0.5
     'AFLT':(PEG17_PHOENIX,(None,29,29,12,54,42,6,0,55),3,None), 
-    #06.09.26 -0.8
-    # 'AFLT2':(LEG2_HOTS,(None,None,43,2.7,6,40,14,0),1,None), 
-    # +16 +0 -0 +5 !(44%) -1.4 -17.5 -2.3 -8.5 -3
-    'ALRS':(LEG2_DRINKER,(None,16,5,2.1,5,31,16,0),2,None),
-    #06.09.26 -2.3
-    # 'ALRS2':(LEG1_CC2,(None,8,5,19,55,5,0.6,0,0,3,0.24),1,None),
+    #20.09.26
+    # 'AFLT2':(LEG1_PIN,(None,19,2,4,19,4),2,None),
+    #20.09.26
+    'ALRS':(PEG18_BLAZE,(None,9,34,11,54,29,23,0,55),1,None),
+    #20.09.26
+    # 'ALRS2':(LEG2_FENNEC,(None,None,3,2.0,23,38,20,1.8,1),1,None),
 
-    #-4 +11 +8 +1 !(40%) +3.1 -7 -1.2 -0.1 -2.4
-    'ASTR':(WEG3_BATYA,(None,40,21,2.1,1,1,1),4,None), 
-    #06.09.26 -4.2
-    # 'ASTR2':(PEG17_PHOENIX,(None,29,10,5,7,35,13,1,55),3,None),
+    #20.09.26
+    'ASTR':(PEG18_UTER2,(None,40,7,3.9,4,15,14,59,10,0,55,1,0),4,None),
+    #20.09.26
+    'ASTR2':(PEG4_UNIVERSAL,(None,19,13,12,29,18,'WC','mfi',5),2,None),
     # +5 -5 +13 +1 !(38%) -2.2 -17.1 +3.5 +1.7 -9
-    'CHMF':(VEG1_VENUS,(None,None,0.3,0.8,0.1,2.0,1,0,0),1,None), 
+    # 'CHMF':(VEG1_VENUS,(None,None,0.3,0.8,0.1,2.0,1,0,0),1,None), 
     #06.09.26
     # 'CHMF2':(PEG18_UTER2,(None,None,51,3.3,6,37,45,85,2,0,55,1,0),1,None),
 
-    #06.09.26 -10 +8.8 +5.8 -16.6 +10.7
-    'FEES':(PEG18_UTER2,(None,None,50,2.4,8,27,45,90,19,0,55,1,1),1,None), #из-за оптимизации по сути тупо WDDCr
-    #06.09.26 -5.3 +0.6
-    # 'FEES2':(LEG1_PIN,(None,13,5,2,28,3),2,None),
-    #06.09.26 +0.3 -3.8 -2.3 -1.9 -1.4
-    'MAGN':(PEG18_DIABLO2,(None,None,14,2.0,29,26,0,55),1,None),
-    #06.09.26
-    # 'MAGN2':(PEG18_UTER2,(None,None,7,3.7,37,16,20,62,6,0,55,1,0),1,None),
+    #20.09.26
+    'FEES':(LEG1_IGOGOSHA2,(None,7,12,42,'rsi_tw',6,0.06,41,55),1,None),
+    #20.09.26
+    # 'FEES2':(WEG3_BATYA,(None,None,14,1.5,1,1,1),1,None),
+    #20.09.26
+    'MAGN':(PEG26_UNKNOWN,(None,None,2,40,28,12,3,34,9,55,0),1,None),
+    #20.09.26
+    # 'MAGN2':(LEG1_BIBI2,(None,None,2,5,'%d',55,5,0.01),1,None),
 
     #+9 +7 -3 !(29%) +0.6 +1.3 +1.6 +4.3 +3.7
     'MTLR':(PEG11_KUSURUKEN,(None,None,44,8,51,36,'c',55),1,None), 
-    #06.09.26
-    # 'MTLR2':(PEG18_REXXAR2,(None,None,3,1.3,39,31,12,0,55),1,None),
-    #06.09.26
-    # 'NLMK':(LEG2_HOTS,(None,None,30,2.4,3,33,22,0),1,None), 
-    #06.09.26
-    # 'NLMK2':(LEG1_PIN,(None,13,4,4,40,3),2,None),
+    #20.09.26
+    'MTLR2':(SEG1_LITE,(None,37,31,2.7,0.2,11),4,None),
+    #20.09.26
+    # 'NLMK':(PEG17_PHOENIX,(None,None,37,16,12,9,18,0,55),1,None),
+    #20.09.26
+    # 'NLMK2':(LEG2_LYNX,(None,None,47,1.0,49,1.3,0),1,None),
 
-    #06.09.26 -1.9 -22 -1.9 -4.2 +5.1
-    'RAGR':(LEG1_BIBI2,(None,None,20,20,'cci',55,4,0.37),1,None),
-    #06.09.26
-    # 'RAGR2':(LEG1_IGOGOSHA2,(None,31,54,12,'williams_r',4,0.24,14,55),3,None),
+    #20.09.26
+    'RAGR':(LEG1_BIBI2,(None,None,6,50,'williams_r',55,2,0.21),1,None),
+    #20.09.26
+    # 'RAGR2':(PEG18_UTER2,(None,None,7,0.6,4,21,42,89,14,1,55,1,1),1,None),
+    #20.09.26
+    'ROSN':(UEG6_ADVENTURE,(None,None,30,47,16,2.5,0),1,None),
     #06.09.26 +0.4 -1.2 +3 +4.4 +1.2
-    'ROSN':(PEG17_PHOENIX,(None,None,8,50,30,48,5,1,55),1,None),
-    #+5 +2 !(33%) -1.3 -2.4
-    # 'ROSN2':(PEG14_RENEGADE,(None,None,16,38,52,6,49,30),1,None), 
+    'ROSN2':(PEG17_PHOENIX,(None,None,8,50,30,48,5,1,55),1,None),
 
-    #06.09.26 +0.9 +0.2 +0.2 0 -7.5
-    'RUAL':(PEG18_UTER,(None,34,19,3,4,39,62,13,1),3,None),
-    #06.09.26 -2.1
-    # 'RUAL2':(PEG17_PHOENIX,(None,None,4,6,9,47,12,0,55),1,None),
-    #06.09.26 -1
-    # 'SBER':(LEG2_HOTS,(None,118,13,3.0,8,33,25,0),10,None),
-    #06.09.26 +1.2 -3.1
-    # 'SBER2':(PEG14_RANGER,(None,227,22,33,6,9,8,86),19,None),
+    #20.09.26
+    'RUAL':(UEG6_PIGEON,(None,None,55,3,38,3,4,0.5,2.5,0),1,None),
+    #20.09.26
+    # 'RUAL2':(LEG1_IRONANNY2,(None,29,47,6,55,4,6,0.24),3,None),
+    #20.09.26
+    'SBER':(UEG6_ADVENTURE,(None,260,7,48,21,2.0,1),22,None),
+    #20.09.26
+    'SBER2':(UEG6_PIGEON,(None,227,8,48,3,5,1,0.38,2.0,0),19,None),
 
-    #06.09.26 +1.7 -4.6 -0.3 -0.4 -0.7
-    'SBERP':(UEG6_PIGEON,(None,124,9,20,37,5,3,0.66,2.4,0),11,None),
-    #06.09.26 +3.2 -0.9
-    # 'SBERP2':(PEG17_PHOENIX,(None,164,29,11,48,27,15,1,55),14,None),
-    #+22 +6 -9 !(65%) -1.8 +5.1 +2.3 -1.6 -4.7
-    'SFIN':(LEG2_DRINKER,(None,None,42,2.0,4,26,11,0),1,None),
-    #+11 -1 !(89%) -3 -9.3
-    # 'SFIN2':(LEG1_PIN,(None,None,4,6,26,3),1,None), 
+    #20.09.26
+    'SBERP':(PEG4_UNIVERSAL,(None,None,13,3,27,19,'VC','s',3),1,None),
+    #20.09.26
+    'SBERP2':(UEG7_PIGEON,(None,None,7,2,55,3,2,0.75,2.1,1),1,None),
+    #20.09.26
+    'SFIN':(LEG2_ALKASH,(None,None,2,2.5,8,0),1,None),
+    #20.09.26
+    # 'SFIN2':(LEG2_LOGAN,(None,23,12,8,19),2,None),
 
-    #06.09.26 -2.5 +0.6 -1 -1.5 -3.1
-    'ENPG':(LEG1_IRONANNY2,(None,59,40,7,55,5,4,0.4),5,None),
-    #06.09.26 -3.9
-    # 'ENPG2':(UEG6_VULTURE,(None,None,25,7,57,52,4,1,0.96,11),1,None),
-    #06.09.26 -1.3
-    # 'SIBN':(PEG18_ANDUIN,(None,None,2,1.7,28,24,5,19,0,55),1,None),
-    #06.09.26 +0.8 -4
-    # 'SIBN2':(PEG17_PHOENIX,(None,None,45,35,5,29,7,0,55),1,None),
+    #20.09.26
+    'ENPG':(PEG4_U3,(None,39,11,3,3,55,'VC','rsi_tw',5),4,None),
+    #20.09.26
+    # 'ENPG2':(LEG1_OKROSHKA,(None,32,5,26),3,None),
+    #20.09.26
+    'SIBN':(LEG2_LYNX,(None,55,10,2.8,55,1.0,1),5,None),
+    #20.09.26
+    'SIBN2':(PEG4_UNIVERSAL,(None,54,35,30,34,6,'BB','s',4),5,None),
 
-    #06.09.26 +2.3 -2.7 +2.5 -4.9 -1
-    'IRAO':(PEG19_ANUBARAK,(None,None,17,3,11,39,43,91,0,55),1,None),
+    #20.09.26
+    'IRAO':(LEG2_FENNEC,(None,None,30,2.8,6,38,11,1.2,0),1,None),
     #06.09.26 -2.2 -4.2
     # 'IRAO2':(LEG1_CC2,(None,None,51,7,55,6,1.6,0,1,2,0.03),1,None),
-    #06.09.26 +1.8 -14 +1 +6.1 +4.9
-    'SNGSP':(LEG1_CC2,(None,74,6,48,55,3,0.5,0,0,2,0.16),7,None),
-    #06.09.26 -2.4 -3.1
-    # 'SNGSP2':(PEG4_UNIVERSAL,(None,72,18,3,23,42,'VC','rsi_tw',6),6,None),
+    #20.09.26
+    'SNGSP':(LEG2_LOGAN,(None,81,12,30,18),7,None),
+    #20.09.26
+    'SNGSP2':(PEG17_PHOENIX,(None,None,7,34,24,54,7,1,55),1,None),
 
-    #+7 +2 +5 !(54%) +5.2 +0.7 -0.2 +4.3 -6.2
-    'SPBE':(PEG11_KUSURUKEN,(None,None,53,8,29,37,'c',55),1,None), 
-    #06.09.26 +4.2 +7.4
-    # 'SPBE2':(UEG6_VULTURE,(None,None,7,32,69,47,3,1,0.07,12),1,None),
-    #06.09.26 -1.2 -3 +1.3 -2.7 +2.2
-    'T':(UEG6_ADVENTURE,(None,None,55,55,22,1.6,1),1,None), #полдня стоял в боковике и на этом не заработал
-    #06.09.26
-    # 'T2':(UEG7_ADVENTURE,(None,122,54,4,55,1.6,1),11,None),
+    #20.09.26
+    'SPBE':(LEG2_DRINKER,(None,7,6,1.8,33,39,38,0),1,None),
+    #20.09.26
+    'SPBE2':(UEG6_VULTURE,(None,9,39,46,54,43,2,3,0.32,7),1,None),
+    #20.09.26
+    'T':(UEG6_ADVENTURE,(None,None,50,51,42,2.6,1),1,None),
+    #20.09.26
+    # 'T2':(PEG4_U3,(None,None,46,44,5,55,'BB','mfi',4),1,None),
 
     #+5 +9 +11 !(14%) +3.4 -2.2 +7.6 -2.2 +0
     'TATN':(LEG1_PIN,(20,None,48,3,39,3),2,None), 
-    #+7 +9 +4 +7 !(9%) +5.5 -10.9
-    # 'TATN2':(WEG4_PUPPY,(None,None,41,39,11),1,None), 
-    #06.09.26 +2.4 +4.2 +3.8 -0.6 -5.1
-    'TATNP':(WEG3_BATYA,(None,None,10,1.9,1,1,1),1,None),
-    #+6 +2 +7 !(24%) -2.6 -10.5
-    # 'TATNP2':(SEG3_FORCE,(None,None,14,3.0,3,9,53,43,0.5),1,None), 
+    #20.09.26
+    # 'TATN2':(PEG4_UNIVERSAL,(None,49,22,13,42,16,'WC','rsi',4),5,None),
+    #20.09.26
+    'TATNP':(UEG6_SHERIFF,(None,None,48,50,2.8),1,None),
+    #20.09.26
+    'TATNP2':(PEG17_PHOENIX,(None,None,16,45,40,49,5,0,55),1,None),
     
+    #20.09.26
+    'VKCO':(WEG3_BATYA,(None,None,45,1.8,1,1,1),1,None),
     #+14 +10 +2 !(46%) -3 -3.1 +2.5 +14.6 +7.5
-    'VKCO':(PEG17_PHOENIX,(None,None,9,12,50,43,26,0,55),1,None), 
-    #06.09.26 -5.6
-    # 'VKCO2':(PEG17_PHOENIX,(None,24,5,6,13,35,19,1,55),2,None),
+    'VKCO2':(PEG17_PHOENIX,(None,None,9,12,50,43,26,0,55),1,None), 
+    #20.09.26
+    'VTBR':(LEG2_LYNX,(None,None,47,2.7,7,2.0,0),1,None),
     #06.09.26 +5 +4.4 +4.8 -5 +0.5
-    'VTBR':(LEG2_FENNEC,(None,83,54,3.0,7,40,24,1.5,0),7,None),
-    #06.09.26 +0.9 -0.1
-    # 'VTBR2':(WEG3_BATYA,(None,None,16,2.1,1,1,1),1,None),
+    'VTBR2':(LEG2_FENNEC,(None,83,54,3.0,7,40,24,1.5,0),7,None),
     
 }
 # sleep_group = ()
